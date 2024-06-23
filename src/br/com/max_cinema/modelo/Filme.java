914b93d10c4d5d370eb0cb42e8f5b8cc19d0da96
tlima1011 <thiagolima1011@gmail.com>
2024-06-23 15:27:32 -0300

    Classe Filme

--- src/br/com/max_cinema/modelo/Filme.java
+++ src/br/com/max_cinema/modelo/Filme.java
@@ -1,13 +1,79 @@
 package br.com.max_cinema.modelo;
 
+import java.util.ArrayList;
+
 public class Filme {
 	
+	private int ultimoCodigo = 1; 
 	private int id_filme; 
 	private String titulo; 
 	private String genero;
 	private String classificacao_etaria; 
 	private int duracao; 
 	private String pais; 
+	ArrayList <Sessao> sessao = new ArrayList<>(); 
+	
+	public Filme() { 
+		id_filme = ultimoCodigo++;
+	}
+
+	public int getId_filme() {
+		return id_filme;
+	}
+
+	public void setId_filme(int id_filme) {
+		this.id_filme = id_filme;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getClassificacao_etaria() {
+		return classificacao_etaria;
+	}
+
+	public void setClassificacao_etaria(String classificacao_etaria) {
+		this.classificacao_etaria = classificacao_etaria;
+	}
+
+	public int getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(int duracao) {
+		this.duracao = duracao;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	public void addSessao(Sessao s) { 
+		addSessao(s); 
+	}
+	
+	public void removeSessao(Sessao s) {
+		removeSessao(s); 
+	}
+	
+	
 	
 
 }
